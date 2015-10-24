f84a1cb96a87b7737b090b9f1d6ff7489de21da1
whitespaceninja <dave@whitespace.ninja>
2015-10-24 13:10:27 -0400

    Adding marker map things

--- js/scripts/app.js
+++ js/scripts/app.js
@@ -1,5 +1,46 @@
 var app = angular.module("proShepherdAdmin", ['uiGmapgoogle-maps'])
 .controller('mapCtrl', function($scope, $timeout) {
-	$timeout(function(){$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };}, 5000);
+	$timeout(function(){
+		$scope.map = {
+	      center: {
+	        latitude: 42.9851,
+	        longitude: -78.6680
+	      },
+	      zoom: 9,
+	      bounds: {}
+	    };
+
+	    $scope.options = {
+	      scrollwheel: false
+	    };
+
+	    var createMarker = function(name, latitude, longitude) {
+	      var ret = {
+	        latitude: latitude,
+	        longitude: longitude,
+	        title: name
+	      };
+	      ret["id"] = name;
+	      return ret;
+	    };
+
+	    $scope.markers = [];
+
+	    // Get the bounds from the map once it's loaded
+	    $scope.$watch(function() {
+	      return $scope.map.bounds;
+	    }, function(nv, ov) {
+	      // Only need to regenerate once
+	      if (!ov.southwest && nv.southwest) {
+	        var markers = [];
+	        markers.push(createMarker('tyler', 42.9851, -78.6680));
+	        markers.push(createMarker('anthony', 42.7851, -78.7680));
+	        markers.push(createMarker('val', 42.8851, -78.8680));
+	        
+	        $scope.markers = markers;
+	      }
+	    }, true);}
+		, 1000
+	);
 });
 
